4257f5b3facdd6cec1de8b1051e4957e80b012f6
Shreyanshsaurabh <66131313+Shreyanshsaurabh@users.noreply.github.com>
2026-09-23 12:15:07 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@ except ImportError:
     RAW_AVAILABLE = False
 
 OUTPUT_FILE = "puzzle.xlsx"
-DB_NAME = os.environ.get("MONGODB_DB", "db2")
+DB_NAME = os.environ.get("MONGODB_DB", "db3")
 
 # ---- goals / filters -------------------------------------------------------
 TARGET_DEVELOPERS = 10_000
